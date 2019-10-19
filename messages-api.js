5b97636141d9da78022a969ba338b37e0eb616f8
EsviCodes <esther.js.vis@gmail.com>
2019-10-19 19:52:00 +0200

    Cleaned up code messages-api

--- messages-api.js
+++ messages-api.js
@@ -2,30 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// WRITE MIDDLEWARE FUNCTION
-// After trying for
-let countRequest = 1;
-
+// MIDDLEWARE FUNCTION THAT LIMITS POST REQUEST TO 5
+let countRequests = 1;
 const postRequestMiddleware = count => (req, res, next) => {
   if (count > 5) {
-    console.count(); // shows me that if post request is ok, this one doesn't get called
     res.status(429).end();
   } else if (req.body.text && req.body.text !== "") {
-    console.count(); // tells me how often this function has been running
-    console.log("COUNT IN MW", count); // I can read countRequests here - this one gets updated
     count++;
   }
   next();
 };
 
-console.log("COUNT OUTSIDE MQ", countRequest); // this doesn't get updated... :-(
-
 app
   .use(bodyParser.json())
-  .use(postRequestMiddleware(countRequest))
-  .post("/messages", (req, res) => {
+  .post("/messages", postRequestMiddleware(countRequests), (req, res) => {
     // Logs the text property of the body to the console
     console.log(`Message sent: ${req.body.text}`);
 
@@ -39,4 +31,4 @@ app
       message: "Message received loud and clear"
     });
   })
-  .listen(port, () => console.log(`Listening on port ${port}!`));
+  .listen(port);
