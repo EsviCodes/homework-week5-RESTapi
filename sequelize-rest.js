07a9bcdb91b5ae7c6e517ae20df960edc8499727
EsviCodes <esther.js.vis@gmail.com>
2019-10-19 17:03:07 +0200

    Added 3 rows of example data in db

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -6,14 +6,38 @@ const databaseUrl =
 
 const db = new Sequelize(databaseUrl);
 
-db.sync({ force: true }) // NOTE: --> use if want to delete db on heroku. Deploy again with sync()
-  //db.sync()
-  .then(() => console.log("Database schema updated"))
-  .catch(console.error);
-
 // Define Data Model Movie
 const Movie = db.define("movie", {
-  title: Sequelize.STRING,
+  title: Sequelize.TEXT,
   yearOfRelease: Sequelize.INTEGER,
-  synopsis: Sequelize.STRING
+  synopsis: Sequelize.TEXT
 });
+
+db.sync()
+  .then(() => {
+    //console.log("Database schema updated");
+
+    // Example Data
+    // Instead of using the create() method which I have to apply three times, I chose to use the method bulkCreate() to insert three rows at once.
+    Movie.bulkCreate([
+      {
+        title: "Beauty and the Beast",
+        yearOfRelease: 2019,
+        synopsis:
+          "Beauty and the Beast focuses on the relationship between the Beast, a prince who is magically transformed into a monster and his servants into household objects as punishment for his arrogance, and Belle, a young woman whom he imprisons in his castle to become a prince again. To break the curse, Beast must learn to love Belle and earn her love in return before the last petal falls from an enchanted rose or else the Beast will remain a monster forever."
+      },
+      {
+        title: "Frozen",
+        yearOfRelease: 2013,
+        synopsis:
+          "Frozen tells the story of a fearless princess who sets off on a journey alongside a rugged iceman, his loyal reindeer, and a naive snowman to find her estranged sister, whose icy powers have inadvertently trapped their kingdom in eternal winter."
+      },
+      {
+        title: "Moana",
+        yearOfRelease: 2016,
+        synopsis:
+          "The film tells the story of Moana, the strong-willed daughter of a chief of a Polynesian village, who is chosen by the ocean itself to reunite a mystical relic with the goddess Te Fiti. When a blight strikes her island, Moana sets sail in search of Maui, a legendary demigod, in the hope of returning the relic to Te Fiti and saving her people."
+      }
+    ]);
+  })
+  .catch(console.error);
